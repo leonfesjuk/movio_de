40c4507b4babda007efb7b85701fc94294866354
orekhov-vitaly <orekhov.vitaly@gmail.com>
2026-04-14 21:48:36 +0200

    Added PasswordValidator + Unit-tests

---
+++ be/src/main/java/de/upteams/tasktracker/validation/password/ValidPassword.java
@@ -0,0 +1,19 @@
+package de.upteams.tasktracker.validation.password;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PasswordValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidPassword {
+
+    String message() default "{user.password.default}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
