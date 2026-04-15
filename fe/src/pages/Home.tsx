e977d9921936a80305b7d7032672a1d0bf7a87b6
Kharchenko_Niks <veronikkharchenko9@gmail.com>
2026-04-15 15:36:48 +0200

    feat: integrate SessionCard into Home page

--- fe/src/pages/Home.tsx
+++ fe/src/pages/Home.tsx
@@ -1,19 +1,31 @@
-import FilterBar from "@/components/common/FilterBar.tsx";
+import FilterBar from "@/components/common/FilterBar";
+import {SessionCard} from "@/features/SessionCard/SessionCard";
+
+
 export default function Home() {
   return(
       <div className="p-10">
+
         <h1 className="text-2xl font-bold mb-6">
           Movie Sessions
         </h1>
 
-        <FilterBar/>
+          <div className="mb-6">
+              <FilterBar/>
+          </div>
 
         <div className="grid grid-cols-4 gap-6">
-          <div className="h-[250px] by-gray-200 rounder-xl"/>
-          <div className="h-[250px] by-gray-200 rounder-xl"/>
-          <div className="h-[250px] by-gray-200 rounder-xl"/>
-          <div className="h-[250px] by-gray-200 rounder-xl"/>
+
+            <SessionCard
+                session={{
+                    id:"1",
+                    title: "Avatar",
+                    time: "18:00",
+                    notificationsSent: false
+                }}
+                isAuth={true}
+            />
         </div>
       </div>
-  );
+  )
 }
