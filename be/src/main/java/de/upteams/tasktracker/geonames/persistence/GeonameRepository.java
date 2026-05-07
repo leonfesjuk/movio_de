a4d2e65260626ef05d15c511a9e9a18bf545f45a
Alexandr Eriman <de.eriman.de@gmail.com>
2026-05-07 15:48:58 +0300

    Fast fix BD Query GeonameRepository

--- be/src/main/java/de/upteams/tasktracker/geonames/persistence/GeonameRepository.java
+++ be/src/main/java/de/upteams/tasktracker/geonames/persistence/GeonameRepository.java
@@ -12,15 +12,13 @@ import java.util.List;
 @Repository
 public interface GeonameRepository extends JpaRepository<GeonameEntity, Long> {
 
-    @Query("SELECT g FROM GeonameEntity g " +
-           "WHERE g.isActive = true " +
-           "AND (LOWER(g.name) LIKE LOWER(CONCAT(:q, '%')) " +
-           "OR LOWER(g.asciiName) LIKE LOWER(CONCAT(:q, '%'))) " +
-           "ORDER BY g.population DESC")
+    @Query(value = "SELECT g FROM GeonameEntity g " +
+            "WHERE (LOWER(g.name) LIKE LOWER(CONCAT(:q, '%')) " +
+            "OR LOWER(g.asciiName) LIKE LOWER(CONCAT(:q, '%'))) " +
+            "ORDER BY g.population DESC")
     List<GeonameEntity> searchAll(@Param("q") String query, Pageable pageable);
 
     @Query("SELECT DISTINCT g FROM GeonameEntity g " +
-           "JOIN Cinema c ON c.geonameId = g.id " +
            "WHERE g.isActive = true " +
            "AND (LOWER(g.name) LIKE LOWER(CONCAT(:q, '%')) " +
            "OR LOWER(g.asciiName) LIKE LOWER(CONCAT(:q, '%'))) " +
