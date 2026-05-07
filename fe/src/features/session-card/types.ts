9740e2315b6f01fffb094df68c6e7354a1289bf7
Kharchenko_Niks <veronikkharchenko9@gmail.com>
2026-05-07 22:57:36 +0200

    FE-122: add session fields and cinema select

--- fe/src/features/session-card/types.ts
+++ fe/src/features/session-card/types.ts
@@ -1,15 +1,21 @@
 export type Session = {
-    id: string
-    title: string
-    city:string
-    date: string
-    time: string
-    datetime?: string
-    description?: string
-    notificationsSent: boolean
+    id: string;
+    title: string;
+    city: string;
+    date: string;
+    time: string;
+    datetime?: string;
+    description?: string;
     imageUrl?: string;
     externalUrl?: string;
-}
+    cinemaId?: string;
+    notificationsSent: boolean;
+    timeFlags?: {
+        timeFlag1: boolean;
+        timeFlag2: boolean;
+        timeFlag3: boolean;
+    };
+};
 
 export type SessionCardActionsProps = {
     session: Session
@@ -27,11 +33,21 @@ export type SessionCardProps = {
 
 export type ApiSession = {
     id: number;
-    title:string;
-    datetime: string;
+    title: string;
+    description?: string;
     imageUrl?: string;
     seanceLink?: string;
+    datetime: string;
+
+    cinemaId?: string;
+
+    timeFlags?: {
+        timeFlag1: boolean;
+        timeFlag2: boolean;
+        timeFlag3: boolean;
+    };
+
     cinema: {
-        cityName:string;
-    }
+        cityName: string;
+    };
 };
